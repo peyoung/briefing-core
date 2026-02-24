7839e3093f994b38e0a3d78d1a7daf7efd9bc2e4
乾慎登 <inui@conscious.jp>
2026-02-24 12:27:40 +0900

    動画設置

--- src/scripts/smooth-scroll.js
+++ src/scripts/smooth-scroll.js
@@ -35,39 +35,39 @@ document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
 
 // ページ外スクロール
 // ページが読み込まれたら処理を実行
-window.addEventListener('DOMContentLoaded', function () {
-  // URLオブジェクトを使用して、現在のURLを解析し、ハッシュ値を取得
-  const url = new URL(location.href);
+// window.addEventListener('DOMContentLoaded', function () {
+//   // URLオブジェクトを使用して、現在のURLを解析し、ハッシュ値を取得
+//   const url = new URL(location.href);
 
-  // slice()メソッドを使って、ハッシュ値の先頭の#を除去
-  const hash = url.hash.slice(1);
+//   // slice()メソッドを使って、ハッシュ値の先頭の#を除去
+//   const hash = url.hash.slice(1);
 
-  //取得したハッシュ値をIDとして持つ要素を取得
-  const target = document.getElementById(hash);
+//   //取得したハッシュ値をIDとして持つ要素を取得
+//   const target = document.getElementById(hash);
 
-  //targetに対応する要素が存在する場合、以下の処理を実行
-  if (target) {
-    //getBoundingClientRect()を呼び出して、スクロール先の要素の位置を取得。また、headerの高さを引いて、スクロール位置がヘッダーの下になるように調整
-    const top = target.getBoundingClientRect().top + window.scrollY - headerHeight;
+//   //targetに対応する要素が存在する場合、以下の処理を実行
+//   if (target) {
+//     //getBoundingClientRect()を呼び出して、スクロール先の要素の位置を取得。また、headerの高さを引いて、スクロール位置がヘッダーの下になるように調整
+//     const top = target.getBoundingClientRect().top + window.scrollY - headerHeight;
 
-    //setTimeout()を使って、スクロール位置を初期化
-    setTimeout(function () {
-      window.scrollTo({ top: 0 }, 0);
-    });
+//     //setTimeout()を使って、スクロール位置を初期化
+//     setTimeout(function () {
+//       window.scrollTo({ top: 0 }, 0);
+//     });
 
-    //setTimeout()を使って、スクロール位置を設定します。この処理は、先程と同じ方法でスクロール位置を計算し、window.scrollTo()メソッドを呼び出して、スムーズなスクロールを実現
-    setTimeout(function () {
-      if (window.lenis) {
-        window.lenis.scrollTo(top, {
-          offset: -headerHeight,
-          immediate: true,
-        });
-      } else {
-        window.scrollTo({
-          top: top,
-          behavior: 'smooth',
-        });
-      }
-    }, 1000);
-  }
-});
+//     //setTimeout()を使って、スクロール位置を設定します。この処理は、先程と同じ方法でスクロール位置を計算し、window.scrollTo()メソッドを呼び出して、スムーズなスクロールを実現
+//     setTimeout(function () {
+//       if (window.lenis) {
+//         window.lenis.scrollTo(top, {
+//           offset: -headerHeight,
+//           immediate: true,
+//         });
+//       } else {
+//         window.scrollTo({
+//           top: top,
+//           behavior: 'smooth',
+//         });
+//       }
+//     }, 1000);
+//   }
+// });
